12608fa9bed802113fca6552b9c8b9045f56cffc
Deepjyoti <deep.barman30@gmail.com>
2017-12-30 00:04:14 +0530

    Animation
    A loading animation is added when the playlist is being created.

--- Shusic.py
+++ Shusic.py
@@ -1,8 +1,25 @@
+import time
+import itertools
+import _thread
 import os
+import sys
 from random import randint
-path = input("Enter the path.\n")
+
+path = input("\t\t\t\t\t\tEnter the path.\n\t\t\t\t\t\t")
 f = open(path+"\log", "w+")
 f.close()
+
+done = False
+print("\n\n")
+
+def animate():                                                       #Animation of loading
+    for c in itertools.cycle(["|", "/", "--", "\\"]):
+        if done:
+            break
+        sys.stdout.write("\r\t\t\t\t\tMaking a queue " + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.flush()
 
 def countFiles():      
     noOffiles = 0                                 #Will count the no of files present in the folder
@@ -25,7 +42,6 @@ def Check(gen_no):                                      #Check if the no is alre
             return False
 
 def GenerateList():                                         #Generates the playing queue
-        print("Loading....")
         while True:
             na = int(countFiles())
             if na == 0:
@@ -35,7 +51,6 @@ def GenerateList():                                         #Generates the playi
             while True:                                                                                  
                 i = randint(0,na)
                 if count == na:
-                    input("\nDone Loading! Press any key to Start!")
                     return True
                 if Check(i):
                     log = open(path+"\log", "a")
@@ -59,19 +74,23 @@ def Play():                                                         #Plays song 
             for files in os.listdir(path):
                 if files.endswith(".mp3"):
                     if str(i) == no[:pos]:
-                        print("\nYou will listen to  "+files)
+                        print("\n\t\t\tYou will listen to  "+files)
                         os.startfile(path+"\\"+files)
                         break
                     else:
                         i += 1
-        user = input("\nPress any key to play next and exit to exit the script")
+        user = input("\n\t\t\tPress any key to play next and exit to exit the script ")
         if user == "exit":
             log.close()
             os.remove(path+"\log")
             return 0
 
 countFiles()
+_thread.start_new_thread(animate, ())
 if GenerateList() == True:
+    done = True
+    input("\n\n\t\t\t\t\t    Press any key to start")
     Play()
+    print("\t\t\t\t")
     os.system("TASKKILL /F /IM Music.UI.exe")
-input("Thanks for using the script! Hope you liked it!")
+input("\t\t\tThanks for using the script! Hope you liked it!")
